e0f18417bfacda821283d1fe655db715252c4523
milosz <miloszdrew@gmail.com>
2025-09-26 22:07:15 +0200

    Finished responsive hero

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Hero } from './hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Hero],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('habit-tracker');
