9d58e139030cfdd2d45307afabac84815a266d6d
milosz <miloszdrew@gmail.com>
2025-09-27 18:29:46 +0200

    Added about section

--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Nav } from '../components/nav/nav';
 import { Hero } from '../components/hero/hero';
+import { About } from '../components/about/about';
 
 @Component({
   selector: 'app-home-page',
-  imports: [Nav, Hero],
+  imports: [Nav, Hero, About],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
